529efa94c82d5e34e875342a00f469d6412c4216
portalBlock <portalBlock@users.noreply.github.com>
2023-10-08 22:15:01 -0500

    Testing a new detection method.

--- unflipper/unflipper.py
+++ unflipper/unflipper.py
@@ -3,6 +3,12 @@ import random
 import discord
 from redbot.core import commands, Config, checks
 
+flips = [
+    "(╯°□°)╯︵ ┻━┻",
+    "(ノಠ益ಠ)ノ彡┻━┻",
+    "(┛ಠ_ಠ)┛彡┻━┻"
+]
+
 
 class UnflipperCog(commands.Cog):
     """Unflipper Cog"""
@@ -14,7 +20,8 @@ class UnflipperCog(commands.Cog):
         guild_default_config = {
             "enabled": False,
             "random": True,
-            "random_percent": 0.25
+            "random_percent": 0.25,
+            "enhanced_users": []
         }
 
         self.config.register_guild(**guild_default_config)
@@ -25,13 +32,26 @@ class UnflipperCog(commands.Cog):
             return
         if not await self.config.guild(message.guild).enabled():
             return
-        if "(╯°□°)╯︵ ┻━┻" in message.content or "(ノಠ益ಠ)ノ彡┻━┻" in message.content or "(┛ಠ_ಠ)┛彡┻━┻" in message.content:
-            if await self.config.guild(message.guild).random():
-                conf_percent = await self.config.guild(message.guild).random_percent()
-                percentage_chance = 1 - conf_percent
-                if random.random() < percentage_chance:
-                    return
-            await message.channel.send(content="┬─┬ノ( º _ ºノ)")
+        unflip = False
+        async with self.config.guild(message.guild).enhanced_users() as users:
+            notascii = 0
+            if message.author.id in users:
+                for c in message.content:
+                    if ord(c) > 127:
+                        notascii += 1
+                if notascii / len(message.content) > 0.5:
+                    unflip = True
+            else:
+                for flip in flips:
+                    if flip in message.content:
+                        unflip = True
+            if unflip:
+                if await self.config.guild(message.guild).random():
+                    conf_percent = await self.config.guild(message.guild).random_percent()
+                    percentage_chance = 1 - conf_percent
+                    if random.random() < percentage_chance:
+                        return
+                await message.channel.send(content="┬─┬ノ( º _ ºノ)")
 
     @commands.guild_only()
     @checks.mod()
@@ -60,3 +80,24 @@ class UnflipperCog(commands.Cog):
         """Enables or disables the cog for the guild. [p]unflipper random percent <percent as integer>"""
         await self.config.guild(ctx.guild).random_percent.set(percent / 100)
         await ctx.channel.send(f"Unflipper random percent: {percent}%")
+
+    @_unflipper.group(name="enhanced")
+    async def _unflipper_enhanced(self, ctx: commands.Context):
+        pass
+
+    @_unflipper_enhanced.command("add")
+    async def _unflipper_enhanced_add(self, ctx: commands.Context, user: discord.Member):
+        """Adds a user to the enhanced detection list for the guild."""
+        async with self.config.guild(ctx.guild).enhanced_users() as users:
+            if user.id not in users:
+                users.append(user.id)
+                await ctx.send("User added to enhanced detection!")
+            else:
+                await ctx.send("User already in enhanced detection list.")
+
+    @_unflipper_enhanced.command("delete")
+    async def _unflipper_enhanced_delete(self, ctx: commands.Context, user: discord.Member):
+        """Deletes a user from the enhanced detection list for the guild."""
+        async with self.config.guild(ctx.guild).enhanced_users() as users:
+            users.remove(user.id)
+        await ctx.send("User removed from enhanced detection!")
