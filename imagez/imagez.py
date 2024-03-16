c95c589c97a3b06e681a5647b895f2679b2b45f5
portalBlock <portalBlock@users.noreply.github.com>
2024-03-16 12:35:19 -0500

    Async all the things.

--- imagez/imagez.py
+++ imagez/imagez.py
@@ -18,17 +18,17 @@ class ImagezCog(commands.Cog):
         self.config = ImagezConfigHelper(self._base_path)
 
     @commands.group("imagez", pass_context=True)
-    def _imagez(self, ctx: commands.Context):
+    async def _imagez(self, ctx: commands.Context):
         """Manages the imagez data"""
         pass
 
     @_imagez.group("font")
-    def _imagez_font(self):
+    async def _imagez_font(self):
         """Manages imagez fonts."""
         pass
 
     @_imagez_font.command("download")
-    def _imagez_font_download(self, ctx: commands.Context, name: str, link: str):
+    async def _imagez_font_download(self, ctx: commands.Context, name: str, link: str):
         """Downloads a font for use in images."""
         if not link.startswith("https://") or not link.endswith(".ttf"):
             await ctx.send("Error! You need to specify an HTTPS link ending with `.ttf`.")
@@ -37,6 +37,6 @@ class ImagezCog(commands.Cog):
         await ctx.send(f"Downloaded `{wrapper.name}` as `{wrapper.guid}`.")
 
     @_imagez_font.command("delete")
-    def _imagez_font_delete(self, ctx: commands.Context, name: str):
+    async def _imagez_font_delete(self, ctx: commands.Context, name: str):
         """Deletes a font from the system."""
         pass
