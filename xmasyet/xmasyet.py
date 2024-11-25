67a5777989ea42f4c075866b8753c2a6bbe1815c
portalBlock <portalBlock@users.noreply.github.com>
2024-11-24 19:47:26 -0600

    Try a different time object

--- xmasyet/xmasyet.py
+++ xmasyet/xmasyet.py
@@ -1,6 +1,4 @@
 import datetime
-import os
-from datetime import time
 
 from discord.ext import tasks
 from redbot.core import commands, Config, checks
@@ -9,6 +7,22 @@ from redbot.core.bot import Red
 
 class XMASYetCog(commands.Cog):
     """XMASYet Cog"""
+
+    def __init__(self, bot: Red):
+        self.bot = bot
+        self.config = Config.get_conf(self, identifier=1289862744207523841005)
+
+        global_default_config = {
+            "debug": "",
+            "channels": {}
+        }
+
+        self.config.register_global(**global_default_config)
+
+        self.ask_xmasyet.start()
+
+    def cog_unload(self) -> None:
+        self.ask_xmasyet.cancel()
 
     async def debug(self, message: str):
         out = self.bot.get_channel(int(await self.config.debug()))
@@ -31,22 +45,6 @@ class XMASYetCog(commands.Cog):
                         await self.debug(f'[XMASYet] None channel: {cid}')
                 else:
                     await self.debug(f'[XMASYet] Disabled channel: {cid}')
-
-    def __init__(self, bot: Red):
-        self.bot = bot
-        self.config = Config.get_conf(self, identifier=1289862744207523841005)
-
-        global_default_config = {
-            "debug": "",
-            "channels": {}
-        }
-
-        self.config.register_global(**global_default_config)
-
-        self.ask_xmasyet.start()
-
-    def cog_unload(self) -> None:
-        self.ask_xmasyet.cancel()
 
     @commands.guild_only()
     @checks.mod()
