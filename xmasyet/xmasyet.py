8829e43e215e51d9c6f90e6faf1785113d010055
portalBlock <portalBlock@users.noreply.github.com>
2024-11-24 18:56:11 -0600

    Debug XMASYet

--- xmasyet/xmasyet.py
+++ xmasyet/xmasyet.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime, time
+from datetime import time
 
 from discord.ext import tasks
 from redbot.core import commands, Config, checks
@@ -11,11 +11,16 @@ class XMASYetCog(commands.Cog):
     @tasks.loop(time=time(hour=11, minute=59))
     async def ask_xmasyet(self):
         async with self.config.channels() as channels:
+            print(f'[XMASYet] Firing!')
             for cid, val in channels.items():
                 if val:
                     c = self.bot.get_channel(cid)
                     if c is not None:
                         await c.send("Is it Christmas yet?")
+                    else:
+                        print(f'[XMASYet] None channel: {cid}')
+                else:
+                    print(f'[XMASYet] Disabled channel: {cid}')
 
     def __init__(self, bot: Red):
         self.bot = bot
@@ -36,8 +41,15 @@ class XMASYetCog(commands.Cog):
         pass
 
     @_xmasyet.command("enable")
-    async def xmasyet_enable(self, ctx: commands.Context, enable: bool):
-        """Enables or disables the cog for the guild. [p]xmasyet enable <true|false>"""
+    async def _xmasyet_enable(self, ctx: commands.Context, enable: bool):
+        """Enables or disables the cog for the channel. [p]xmasyet enable <true|false>"""
         async with self.config.channels() as channels:
             channels[ctx.channel.id] = enable
         await ctx.channel.send(f"XMASYet enabled: {enable}!")
+
+    @checks.is_owner()
+    @_xmasyet.command("set")
+    async def _xmasyet_set(self, ctx: commands.Context, hour: int, minute: int):
+        """Updates the daily run time. [p]xmasyet set <hours> <minutes>"""
+        self.ask_xmasyet.change_interval(time=time(hour=hour, minute=minute))
+        await ctx.channel.send(f"XMASYet run time updated!")
